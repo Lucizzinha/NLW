cb8943a2bc880ffc5230fccb1cdf5b374336b1de
Luciane Backes Pedroso <lucianeb.pedroso@hotmail.com>
2025-07-10 22:48:36 -0300

    refactor: remove commented-out API key from script.js

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const markdownToHTML = (text) => {
     return converter.makeHtml(text)
 }
 
-
-// AIzaSyDoEytsFHgqlihxR7pqBXQxmI9TeTM5Koo
 const perguntarAI = async (question, game, apiKey) => {
  const model = 'gemini-2.0-flash'
  const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
